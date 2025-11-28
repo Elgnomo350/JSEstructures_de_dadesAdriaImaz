348fef870d007c2639e41320936ccb30ece6f6e2
Adria Imaz <adriaimaz03@gmail.com>
2025-11-28 17:46:01 +0100

    ex1

--- js/ex1/ex1.js
+++ js/ex1/ex1.js
@@ -5,7 +5,7 @@ for (let i = 0; i < 5; i++) {
 }
 
 console.log("Original:", nums);
-nums.sort((a, b) => a - b);
+nums.sort((a, b) => a - b); //esto lo he buscado en la red
 let primer = nums[0];
 let ultim = nums[nums.length - 1];
 
